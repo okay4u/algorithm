bdb702c486feadd09ec205208e457599dab07ce6
KunWoong <49337490+KunWoong@users.noreply.github.com>
2021-02-12 00:30:37 +0900

    Update step2_math1.py

--- KW/step2_math1.py
+++ KW/step2_math1.py
@@ -4,15 +4,9 @@ from math import floor
 def math_1712(a,b,c):
     return -1 if a/(c-b) < 0 else floor(a/(c-b)+1)
 
-def math_2869(a,b,c):
-    res = 0
-    while True:
-        c -= a
-        res += 1
-        if c < 0:
-            break
-        c += b
-    return res
+# x>= (v-b)/(a-b)
+def math_2869(a, b, v):
+    return ceil((v - b) / (a - b))
 
 # w:width, h:height, n: Nth room
 def math_10250(h, w, n):
@@ -20,9 +14,16 @@ def math_10250(h, w, n):
     prefix = (n-1)%h + 1
     return str(prefix)+f'{suffix:02}'
 
-
+# 뭐가 틀렸는지 모르겠습니다
+def math_2839(n):
+    y = 0
+    while n-3*y > 0:
+        y += 1
+        if (n-3*y)%5 == 0:
+            return y+(n-3*y)//5
+    return -1
 
 if __name__ == '__main__':
     # print(math_1(3,2,1))
     # print(math_10250(6,12,10))
-    print(math_10250(30, 50, 72))
+#     print(math_10250(30, 50, 72))
