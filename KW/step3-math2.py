0ff14d35166c9704be497ab2d5e645c6f6add669
KunWoong <49337490+KunWoong@users.noreply.github.com>
2021-02-20 13:00:15 +0900

    Update step3-math2.py

--- KW/step3-math2.py
+++ KW/step3-math2.py
@@ -29,15 +29,14 @@ def math_1929(m, n):
 
 
 def math_1002(x1, y1, r1, x2, y2, r2):
-    d = sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-
-    if abs(r1 - r2) < d < r1 + r2:
+    d = sqrt((x1-x2)**2 + (y1-y2)**2)
+    
+    if d == 0 and r1 == r2:
+        return -1
+    
+    if abs(r1-r2) < d and d < abs(r1+r2):
         return 2
 
-    if r1 + r2 == d or abs(r1 - r2) == d:
-        return 1
-
-    if x1 == x2 and y1 == y2 and r1 == r2:
-        return -1
-
+    if abs(r1+r2) == d or abs(r1-r2) == d:
+        return 1   
     return 0
